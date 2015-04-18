b457b27c10208dd99a9fa296dfcd70f8b784c60f
Mohammad Doleh <mdoleh@kent.edu>
2015-04-17 23:29:59 -0400

    Initial commi

---
+++ TabifyAll/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Doleh.TabifyAll
+{
+    static class GuidList
+    {
+        public const string guidTabifyAllPkgString = "fae601ee-b1c8-4c7b-8eaa-20eca0304654";
+        public const string guidTabifyAllCmdSetString = "dd625ce3-f08a-4716-b18e-f77dfc2e16c7";
+
+        public static readonly Guid guidTabifyAllCmdSet = new Guid(guidTabifyAllCmdSetString);
+    };
+}
---
+++ TabifyAll/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Doleh.TabifyAll
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdTabifyAll =        0x100;
+
+
+    };
+}
